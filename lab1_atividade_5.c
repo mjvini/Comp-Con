7c48c1db6450ff960664165c96370f960e4d4106
mjvini <40394180+mjvini@users.noreply.github.com>
2022-04-18 22:21:35 -0300

    Update lab1_atividade_5.c

--- lab1_atividade_5.c
+++ lab1_atividade_5.c
@@ -10,47 +10,59 @@
 
 #define NTHREADS  2 //total de threads a serem criadas
 #define TAM_VET 10000
-int vet2[TAM_VET];
+int vet[TAM_VET];
+int vetor[NTHREADS];
+int parte =0; //Determina a parte do vetor a ser executada
 
 //Função que a thread irá executar
 void * tarefa (void * arg){
-    int vet = * (int *) arg; //Recebe o elemento do vetor vet original
-    vet2[TAM_VET] = vet; 
+    int ident = * (int *) arg;
+    vetor[ident-1] = ident;
 
-    int thread_parte = 0;
+    int thread_parte = parte++;
+    //Executa a parte do vetor de acordo com a thread.
     for(int i = thread_parte * (TAM_VET/2);
      i < (thread_parte + 1) * (TAM_VET/2); i++){
-         vet2[i] *= vet2[i];
+         vet[i] *= vet[i];
     }
-
+    printf("A thread %d está fazendo a parte %d do vetor \n",
+    ident, thread_parte);
+    
     pthread_exit(NULL);
 }
 
 //Função principal
 int main(void) {
-    //Inicialização do vetor com 10_000 elementos
+    //Inicialização do vetor com 10_000 elementos (de 0 à 9999)
     //Para facilitar a vizualização do que está acontecendo
     //vou preencher o vetor com todos os elementos = 2.
     //Desse forma, espera-se que as threas elevem eles
     //ao quadrado para 4.
-    int vet[TAM_VET];
-    int cont = 0; //Verifica o total de elementos preenchidos
-    for(int i =0; i<TAM_VET; i++){
-        vet[i] = 2;
-        cont++;
+    //int vet[TAM_VET];
+    //int cont = 0; //Verifica o total de elementos preenchidos
+    for(int i=0; i<TAM_VET; i++){
+        vet[i] = 2; 
+        //cont++;
     }
-    printf("Total de elementos preenchidos %d", cont);
-    
+    //printa um valor qualquer o vetor antes de ser elevado ao quadrado.
+    printf("valor qualquer do vetor antes. vet[4999] = %d\n", vet[4999]);
+
     pthread_t tid[NTHREADS]; //identificador da thread no sistema
+    int ident[NTHREADS]; //identificador local da thread
     //cria as threads 
-    for(int i=1; i<=NTHREADS; i++) {
-       //vet[i-1] = i;
-       if (pthread_create(&tid[i-1], NULL, tarefa, (void *)&vet[i-1])) 
+    for(int i=0; i<NTHREADS; i++) {
+       ident[i] = i+1;
+       if (pthread_create(&tid[i], NULL,
+        tarefa, (void *)&ident[i])) 
           printf("ERRO -- pthread_create\n");
     }
+
     //espera as threads terminarem 
     for(int i=0; i<NTHREADS; i++) {
        if (pthread_join(tid[i], NULL)) 
           printf("ERRO -- pthread_join\n");
     }
+    //printa um valor qualquer o vetor depois de ser elevado ao quadrado.
+    printf("valor qualquer do vetor depois. vet[3000] = %d\n", vet[3000]);
+
 }
