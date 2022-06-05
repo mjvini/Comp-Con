811b6da3ff18d57f3006b6ddbb0205b2f71cd01c
mjvini <40394180+mjvini@users.noreply.github.com>
2022-06-04 22:32:31 -0300

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -4,10 +4,12 @@
 #include "timer.h"
 
 //Número de threads
-#define NTHREADS  5
+#define NTHREADS 5
 
 //Tamanho dos vetores
-int dim =10;
+int dim = 10;
+
+
 
 //Vetores
 int *vetorSeq;
@@ -21,12 +23,23 @@ void inicializarVetor(int dim){
     for(int i=0; i<dim; i++){
         int num_rand = rand() % 1000;
         vetorSeq[i] = num_rand;    
+        vetorConc[i] = num_rand;    
     }
 
 }
 
+int maiorElemento(int dim){
+    int maiorElemento = 0;
+    for(int i = 0; i < dim; i++){
+        if(maiorElemento < vetorConc[i]){
+            maiorElemento = vetorConc[i];
+        }
+    }
+    return maiorElemento;
+}
+
 //Bubble sort sequencial
-void bubble_sort_sequencial(int dim){
+void bubbleSortSequencial(int dim){
     int aux = 0;
     int swapped; //Break o for se não ocorrer swap.
     for (int i = 0; i < dim - 1; i++){
@@ -46,15 +59,45 @@ void bubble_sort_sequencial(int dim){
     }
 }
 
+void *maxElemento(void *arg);
+
+int* criaSublistas(int maiorNumero, int tamanhoLista, int nThreads) {
+    int coeficiente = maiorNumero / nThreads;
+    int sublistas[nThreads][tamanhoLista];
+
+    for (int i = 0; i < nThreads; i++) {
+        int coeficienteAtual = coeficiente * (i + 1);
+        int coeficienteAnterior = coeficiente * i;
+
+        int sublistaIndex = 0;
+
+        for (int j = 0; j < tamanhoLista; j++) {
+            if (vetorConc[j] <= coeficienteAtual && vetorConc[j] > coeficienteAnterior) {
+                sublistas[i][sublistaIndex] = vetorConc[j];
+
+                sublistaIndex++;
+            };
+        };
+    };
+
+    return sublistas;
+};
+
 void *tarefa(void *arg){
-    int ident = * (int *) arg;
-    int tamBloco = dim/NTHREADS //Tamanho do bloco de cada thread
-    
+    // int maiorElemento = 0;
+    // int id = * (int *) arg;
+    // int tamBloco = dim/NTHREADS; //Tamanho do bloco de cada thread
+    // int inicio = id * tamBloco; //Elemento inicial de cada thread
+    // int fim; //Elemento final(nao processado) do bloco da thread
+    // if(id == NTHREADS-1) fim = dim;
+    // else fim = inicio + tamBloco; //Trata o resto se houver
+
+        
 
     pthread_exit(NULL);
 }
 
-int main(){
+int main() {
     
     //Identificador da thread no sistema
     pthread_t tid[NTHREADS];
@@ -72,11 +115,11 @@ int main(){
        return 1;
     }
 
-    pthread_t tid[NTHREADS]; //Identificador da thread no sistema
+    // pthread_t tid[NTHREADS]; //Identificador da thread no sistema
     int ident[NTHREADS]; //Identificador local da thread
     
     //Cria as threads 
-    for(int i=0; i<NTHREADS; i++) {
+    for(int i = 0; i < NTHREADS; i++) {
        ident[i] = i;
        if (pthread_create(&tid[i], NULL,
         tarefa, (void *)&ident[i])) 
@@ -88,7 +131,7 @@ int main(){
     inicializarVetor(dim);
 
     //Chama o Bubble Sort sequencial 
-    bubble_sort_sequencial(dim);
+    bubbleSortSequencial(dim);
 
     //Printa o vetor
     for(int i = 0; i < dim; i++){
