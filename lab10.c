3d99cb6cc4bf89a4bf42e5bf73989acd0cac1965
mjvini <40394180+mjvini@users.noreply.github.com>
2022-07-07 19:35:52 -0300

    Update lab10.c

--- lab10.c
+++ lab10.c
@@ -11,6 +11,65 @@
 #define L 3 //numero de threads leitoras
 #define E 3 //numero de threads escritoras
 
+/*
+Ativida 1:
+a) Os valores iniciais dos semáforos(em_e, em_l, escr, leit)
+devem ser 1.
+
+b) A primeira thread (leitora ou escritora) pegará o sem_wait(&escr) e isso vai fazer com que as outras threads do tipo oposto não possam executar.
+Porém, as outras threads do seu tipo vão poder executar, pois elas não ficarão presas no sem_wait(&escr)
+//thread leitora
+void * leitor (void * arg) {
+  int *id = (int *) arg;
+  while(1) {
+    printf("Leitora %d quer ler\n", *id);  
+    sem_wait(&em_l); l++;
+      
+    if(l == 1) {
+      sem_wait(&escr); 
+    }
+    sem_post(&em_l);
+    printf("Leitora %d esta lendo\n", *id);  
+    sem_wait(&em_l); l--; 
+    printf("Leitora %d terminou de ler\n", *id);
+    if(l == 0) {
+        sem_post(&escr); 
+    }
+    sem_post(&em_l); 
+    sleep(1);
+  } 
+  free(arg);
+  pthread_exit(NULL);
+}
+
+
+
+//thread leitora
+void * escritor (void * arg) {
+  int *id = (int *) arg;
+  while(1) {
+    printf("Escritora %d quer escrever\n", *id);
+    sem_wait(&em_e); e++; 
+    if(e == 1) {
+      sem_wait(&escr);  
+    }
+    sem_post(&em_e);
+    printf("Escritora %d esta escrevendo\n", *id);
+    sem_wait(&em_e); 
+    e--;
+    printf("Escritora %d terminou de escrever\n", *id);
+    if(e == 0){
+      sem_post(&escr); 
+    }
+    sem_post(&em_e);
+    sleep(1);
+  } 
+  free(arg);
+  pthread_exit(NULL);
+}
+
+*/
+
 sem_t em_e, em_l, escr, leit; //semaforos
 int e=0, l=0; //globais
 
